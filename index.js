4b1ac741bef58c69caf8514036a7525d5d641566
Atsighi Bright <brytebee@gmail.com>
2022-07-07 16:34:00 +0100

    :white_check_mark: Update index.js, main.scss and index.html
    Update the backgroung of tokenDist

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ function drawChart() {
   const options =
     window.innerWidth < 506
       ? { title: "Chart", width: 418, height: 350 }
-      : { title: "Chart representation", width: 650, height: 550 };
+      : {
+          title: "Chart representation",
+          width: 650,
+          height: 550,
+          colors: [
+            "#000",
+            "#e0440e",
+            "#f3b49f",
+            "#000",
+            "#e6693e",
+            "#000",
+            "#e6693e",
+            "#f6c7b6",
+            // "#000",
+            // "#ec8f6e",
+          ],
+          is3D: true,
+        };
 
   const chart = new google.visualization.PieChart(
     document.getElementById("piechart")
